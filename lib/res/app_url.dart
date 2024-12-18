21cf3b6bbcf52042ca1b0b348a7a46a688ec4fa7
Seng-Leaph <sengleaphseang@gmail.com>
2024-12-18 14:09:48 +0700

    tesing

--- lib/res/app_url.dart
+++ lib/res/app_url.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppUrl{
-  static String baseUrl = "${dotenv.env["BASE_URL"]}/";
+  static String baseUrl = "${dotenv.env["BASE_URL"]}";
   static String getCinema = "${baseUrl}cinemas";
   static String getFeatureMovie = "${baseUrl}scheduled-films/featured";
   static String getPromotion = "${baseUrl}promotions?limit=10&offset=0&includeAnnouncement=false";
